9ab52eaec86ebc6afc900fec5fdcac3a815550f7
wreckage0907 <girish.raghav2004@gmail.com>
2024-01-27 15:17:02 +0000

    deploy

--- apps/flask/main.py
+++ apps/flask/main.py
@@ -45,4 +45,4 @@ def gemini():
     print(response.text)
     return jsonify(response.text)
 
-app.run(port=5000, debug=True)
+app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
